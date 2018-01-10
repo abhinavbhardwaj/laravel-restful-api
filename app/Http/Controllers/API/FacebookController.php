f4f0f7b3a3e1e164efb4ef15324b3342c2a51d5a
abhinavbhardwaj <abhinav.bhardwaj@engineer.com>
2018-01-10 13:29:00 +0530

    facebook api setup

---
+++ app/Http/Controllers/API/FacebookController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Facebook;
+use File;
+use Validator;
+
+class FacebookController extends Controller { 
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function searchUserFeeds(Request $request) {
+
+        $input = $request->all();
+        $matchedSearch = array();
+        $accessToken = $request->header('authToken');
+        
+        $receiveFormDate = $input['from_date'];
+        $receiveToDate = $input['to_date'];
+        
+        $fromDate = strtotime($receiveFormDate.' 00:00');
+        $toDate = strtotime($receiveToDate.' 23:59');
+        
+        $searchText = trim($input['search_query']); 
+        try {
+            $response = Facebook::get('/me/feed?limit=20000&since=' . $fromDate . '&until=' . $toDate, $accessToken); //current user all post
+        } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+            return response()->json(['error' => 'some error occur'], \Config::get('constants.status.unauthorized'));
+        }
+
+        $post = $response->getDecodedBody(); 
+        if(count($post['data'])>0){
+            $matchedSearch = $this->getMatchedSearch($post['data'], $searchText);
+        }
+        $return['totalFeeds'] = (isset($post)) ? count($post) : 0;
+        $return['matchedFeeds'] = (isset($matchedSearch)) ? count($matchedSearch) : 0;
+        $return['matchedFeedsData'] = (isset($matchedSearch)) ? $matchedSearch : 0; 
+        //$return['totalFeedsData'] = (isset($post)) ? $post : 0;
+
+        $return['from_date'] = $receiveFormDate;
+        $return['to_date'] = $receiveToDate;
+        $return['search_query'] = $searchText;
+        return response()->json(['success' => $return], \Config::get('constants.status.success'));
+    }
+
+    /**
+     * method to filter search text from a given array value
+     * @param array $feedData all post array 
+     * @param string $searchText 
+     */
+    private function getMatchedSearch($feedData, $searchText) {     
+        $searchResult = array();
+        $searchArray = explode(",", $searchText);  
+        foreach ($feedData as $data) {    
+            $message =  (!empty($data['message']))  ? $data['message'] : $data['story']; 
+            
+            if(empty($message))          
+                continue;
+             
+                //check for the search
+                if (count($searchArray) > 0) {
+                    
+                    foreach ($searchArray as $search) {  
+                        
+                        if (strpos(strtolower($message), trim(strtolower($search))) !== FALSE) { 
+                            $searchResult[] = $data; 
+                        }
+                    }
+                
+                }  
+        }   
+        $searchResult = array_unique($searchResult, SORT_REGULAR); 
+        return $searchResult;
+    }
+    
+     /**
+     * API to create a callback for the call from client
+     * @param Request $request
+     */
+    public function callBack(Request $request) {
+         $input = $request->all();
+         $param = "";
+         $i = 0;
+        foreach($input as $key => $value){
+            $con    =   ($i == 0) ? "?" : "&";
+            $param .= $con .trim($key)."=".trim($value);
+            $i++;
+        }
+        header('location:WYF://oauth-callback/facebook'.$param);die;
+
+    }
+}
--- config/app.php
+++ config/app.php
@@ -164,7 +164,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
+        SammyK\LaravelFacebookSdk\LaravelFacebookSdkServiceProvider::class,
         'Thujohn\Twitter\TwitterServiceProvider',
+        
 
         /*
          * Package Service Providers...
@@ -227,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Facebook' => SammyK\LaravelFacebookSdk\FacebookFacade::class,
         'Twitter' => 'Thujohn\Twitter\Facades\Twitter',
 
     ],
